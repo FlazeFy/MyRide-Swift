c12846d18a4a9fc178d55b5a5fe0fac24635a881
Leonardho Rante S <flazen.edu@gmail.com>
2025-09-02 23:49:54 +0700

    [Mobile] Init Swift

---
+++ MyRide/MyRide/MyRideApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyRideApp.swift
+//  MyRide
+//
+//  Created by Leonardho R Sitanggang on 02/09/25.
+//
+
+import SwiftUI
+
+@main
+struct MyRideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
